32ecaa98c7bf158c6b32f016e617a3926a45a358
rocknegi <rocknegi53@gmail.com>
2019-01-23 11:27:19 +0530

    add form for messages and show uncoming message in browser

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,20 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+
+jQuery('#message-form').on('submit', (e) => {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'user',
+    text: jQuery('[name=message]').val()
+  }, () => {
+
+  });
 });
