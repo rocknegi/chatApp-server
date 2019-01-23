352a4b4142349227ab11423bbb561097e6a7ffd0
rocknegi <rocknegi53@gmail.com>
2019-01-23 22:47:49 +0530

    add styling and auto scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+scrollToBottom = () => {
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageheight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageheight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 
@@ -19,6 +34,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   // console.log('newMessage', message);
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -36,7 +52,7 @@ socket.on('newLocationMessage', (message) => {
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
-
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">My current location</a>')
   // li.text(`${message.from} ${formattedTime}: `);
